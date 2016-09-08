6c0933eb4cbaf67a393b7f80cf1a972d241f37da
John Lendvoy <john@tride.ca>
2016-09-08 15:27:02 -0600

    fixed dropdown on other rows

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-	var formRow = '<div class="row form-group"><div class="col-sm-4"><input class="form-control" name="names[]" type="text"></div><div class="col-sm-2"><input class="form-control" name="widths[]" type="number"></div><div class="col-sm-2"><input class="form-control" name="heights[]" type="number"></div><div class="col-sm-2"><select class="form-control" name="ratios[]"><option value="fit">Fit</option><option value="scale">Scale</option></select></div><div class="col-sm-2"><span class="btn btn-danger delete-row-button">Delete Row</span></div></div>';
+	var formRow = '<div class="row form-group"><div class="col-sm-4"><input class="form-control" name="names[]" type="text"></div><div class="col-sm-2"><input class="form-control" name="widths[]" type="number"></div><div class="col-sm-2"><input class="form-control" name="heights[]" type="number"></div><div class="col-sm-2"><select class="form-control" name="ratios[]"><option value="crop">Crop</option><option value="resize">Maintain Aspect Ratio</option></select></div><div class="col-sm-2"><span class="btn btn-danger delete-row-button">Delete Row</span></div></div>';
 
 	// can add rows
     $( "#add-row-button" ).click( function() {
